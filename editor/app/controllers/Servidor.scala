55748b8f9e90cddcd30aa0c9cea3116a208af269
edumarcal <edumarcalnet@gmail.com>
2016-08-14 11:11:01 -0300

    Implementação da Respota

--- editor/app/controllers/Servidor.scala
+++ editor/app/controllers/Servidor.scala
@@ -7,33 +7,50 @@ import scala.collection.mutable.Queue;
 
 @Singleton
 class Servidor @Inject() extends Controller {
-  var arquivo = new Queue[Char]
-  var pedidos = new Queue[Char]
-  var rq = new Queue[AnyContent]
-  //pedidos+=('a','\n', 'b','\\','c','\t','d')
-  
-  def getMessage =  Action {
-    //Ok(views.html.codigo(pedidos))
-    //Ok(views.html.teste(pedidos))
-    Ok(views.html.teste(rq))
-  }
+    var pedidos = new Queue[Char] //Codigo
+    var requisicao = new Queue[AnyContent] //Empilha todos os pedidos
 
-  def postMessage = Action { implicit request =>
-    rq+=(request.body); Redirect(routes.HomeController.index)
-  }
-/*  
+    val thread = new Thread {
+        override def run {
+            if(requisicao != Nil) {
+                while(!requisicao.isEmpty) {
+                    val element = requisicao.dequeue;
+                    val caracter = element.asFormUrlEncoded.map { x => x("caracter")(0).toInt}.getOrElse(-1) 
+          	        val position = element.asFormUrlEncoded.map { x => x("position")(0).toInt}.getOrElse(-1)
+          	        val action = element.asFormUrlEncoded.map { x => x("action")(0)}.getOrElse("") 
+          	
+          	        if(position != -1 && position >= pedidos.size)
+          	            pedidos+=(caracter.toChar)
+          	        
+          	        println("Thread--------------------------------------------------------")
+                    println(pedidos)
+                    println("--------------------------------------------------------------")
+                }
+            }
+        }
+    }
+    
+    def resultMessage() = {
+       while(!requisicao.isEmpty) {
+            val element = requisicao.dequeue;
+            val caracter = element.asFormUrlEncoded.map { x => x("caracter")(0).toInt}.getOrElse(-1) 
+            val position = element.asFormUrlEncoded.map { x => x("position")(0).toInt}.getOrElse(-1)
+            val action = element.asFormUrlEncoded.map { x => x("action")(0)}.getOrElse("") 
+    
+            if(position != -1 && position >= pedidos.size)
+                pedidos+=(caracter.toChar)
+        }
+        println("Thread--------------------------------------------------------")
+        println(pedidos)
+        println("--------------------------------------------------------------")
+    }
+
+    def getMessage =  Action {
+        //thread.start; Ok(views.html.teste(pedidos))
+        resultMessage; Ok(views.html.teste(pedidos))
+    }
+
     def postMessage = Action { implicit request =>
-  //AnyContentAsFormUrlEncoded(Map(caracter -> ArrayBuffer(83), positon -> ArrayBuffer(5), action -> ArrayBuffer(insert)))
-    val caracter = request.body.asFormUrlEncoded.map { x => x("caracter")(0).toInt}.getOrElse(-1) 
-  	val position = request.body.asFormUrlEncoded.map { x => x("position")(0).toInt}.getOrElse(-1)
-  	val action = request.body.asFormUrlEncoded.map { x => x("action")(0)}.getOrElse("") 
-  	
-  	if(position != -1 && position > pedidos.size)
-  	    pedidos+=(caracter.toChar)
-  	    
-  	println(pedidos(position))
-  	
-    println(request.body); Redirect(routes.HomeController.index)
-  }
-  */
+        requisicao+=(request.body); Redirect(routes.HomeController.index)
+    }
 }
